648324afd538d8eb406bcdc19257238295051e07
Anderson Kloss Maia <anderson_maia@sicredi.com.br>
2020-09-05 13:27:18 -0300

    Remove Coordinator file

--- MeuDeputado/AppDelegate.swift
+++ MeuDeputado/AppDelegate.swift
@@ -1,15 +1,17 @@
+import Foundation
 import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-  
   var window: UIWindow?
   
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    defer { window?.makeKeyAndVisible() }
+    
     self.window = UIWindow(frame: UIScreen.main.bounds)
     let viewController = ViewController()
     window?.rootViewController = viewController
-    window?.makeKeyAndVisible()
+    
     
     return true
   }
