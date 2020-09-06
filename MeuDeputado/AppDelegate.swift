7f43cc0c9dfce9f0e9af54feb559260b95525e16
Anderson Kloss Maia <anderson_maia@sicredi.com.br>
2020-09-05 23:03:18 -0300

    Add cocopods-keys

--- MeuDeputado/AppDelegate.swift
+++ MeuDeputado/AppDelegate.swift
@@ -5,7 +5,10 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   
-  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
     defer { window?.makeKeyAndVisible() }
     
     self.window = UIWindow(frame: UIScreen.main.bounds)
--- Podfile
+++ Podfile
@@ -1,4 +1,10 @@
-platform :ios, '12.0'
+platform :ios, '13.0'
+
+plugin 'cocoapods-keys', {
+  :project => "MeuDeputado",
+  :keys => [
+    "MeuDeputadoAPIClientKey"
+]}
 
 target 'MeuDeputado' do
   use_frameworks!
@@ -6,10 +12,12 @@ target 'MeuDeputado' do
   # Pods for MeuDeputado
   pod 'RxSwift', '~> 5'
   pod 'RxCocoa', '~> 5'
+  pod 'Parse', '~> 1'
 
   target 'MeuDeputadoTests' do
     pod 'RxBlocking', '~> 5'
-    pod 'RxTest', '~> 5
-  end
+    pod 'RxTest', '~> 5'
 
+  end
+  
 end
