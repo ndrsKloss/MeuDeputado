3e0842a3f90b59fb3a577ef4b346f8b21aa80838
Anderson Kloss Maia <anderson_maia@sicredi.com.br>
2020-10-30 17:07:25 -0300

    Add all parties query

--- MeuDeputado/Repository/Queries.swift
+++ MeuDeputado/Repository/Queries.swift
@@ -8,3 +8,9 @@ let getAllDeputiesInMandateQuery: PFQuery<PFObject>? = {
 	$0?.whereKey(NSExpression(forKeyPath: \Deputy.updatedAt).keyPath, greaterThan: Date(timeIntervalSince1970: 1603983600))
 	return $0
 }(Deputy.query())
+
+let getAllParties: PFQuery<PFObject>? = {
+	$0?.addAscendingOrder(NSExpression(forKeyPath: \Party.deputyCount).keyPath)
+	$0?.whereKey(NSExpression(forKeyPath: \Party.deputyCount).keyPath, greaterThan: 0)
+	return $0
+}(Party.query())
